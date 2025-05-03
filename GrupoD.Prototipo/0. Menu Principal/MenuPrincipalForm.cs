b5605ac3bedbd13f710e58fc45e73dac3f630a1d
k-solis <solischkaren@gmail.com>
2025-05-03 10:26:25 -0300

    act

--- GrupoD.Prototipo/0. Menu Principal/MenuPrincipalForm.cs
+++ GrupoD.Prototipo/0. Menu Principal/MenuPrincipalForm.cs
@@ -1,4 +1,5 @@
 ﻿using GrupoD.Prototipo.CDU2._GenerarOrdenSeleccion;
+//using GrupoD.Prototipo._4._Empaquetar_Productos;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -27,6 +28,12 @@ namespace GrupoD.Prototipo._0._Menu_Principal
         {
             GenerarOrdenDeSeleccionForm generarOrdenDeSeleccionForm = new GenerarOrdenDeSeleccionForm();
             generarOrdenDeSeleccionForm.Show();
+        }
+
+        private void EmpaquetarProductosMenuBTN_Click(object sender, EventArgs e)
+        {
+            //    GenerarOrdenPreparadaForm generarOrdenPreparadaForm = new GenerarOrdenPreparadaForm();
+            //    generarOrdenPreparadaForm.Show();
         }
 
         private void GenerarOPMenuBTN_Click(object sender, EventArgs e)
