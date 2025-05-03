9b227dd9638a538f5c9b3b2887e39925424208bf
k-solis <solischkaren@gmail.com>
2025-05-03 14:13:02 -0300

    actualizacion

--- GrupoD.Prototipo/4. EmpaquetarProductos/OrdenPreparacion.cs
+++ GrupoD.Prototipo/4. EmpaquetarProductos/OrdenPreparacion.cs
@@ -9,25 +9,20 @@ namespace GrupoD.Prototipo._4._EmpaquetarProductos
 {
     class OrdenPreparacion
     {
-
-        
         // Atributos.
         public int NumeroOrden { get; set; }
         public List<Producto> Productos { get; set; } = new List<Producto>();
         
 
         // Constructor.
-        public OrdenPreparacion(int numeroOrden, string skuProducto, string nombreProducto, int cantidadProducto)
+        public OrdenPreparacion(int numeroOrden)
         {
             NumeroOrden = numeroOrden;
-            SKUProducto = skuProducto;
-            NombreProducto = nombreProducto;
-            CantidadProducto = cantidadProducto;
         }
 
         public override string ToString()
         {
-            return $"{NumeroOrden} - {SKUProducto} - {NombreProducto} - {CantidadProducto}";
+            return $"{NumeroOrden}";
         }
     }
 }
