7bcce3b612e1b96ba2c290a17fb70ba5c481024f
k-solis <solischkaren@gmail.com>
2025-05-03 02:03:37 -0300

    actualizacion carpeta

---
+++ GrupoD.Prototipo/4. Empaquetar Productos/GenerarOrdenPreparadaForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GrupoD.Prototipo._4._Empaquetar_Productos
+{
+    public partial class GenerarOrdenPreparadaForm : Form
+    {
+        public GenerarOrdenPreparadaForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
