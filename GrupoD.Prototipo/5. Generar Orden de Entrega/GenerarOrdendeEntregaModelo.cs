fec56f57cf11b5a5f5693eda5ec2703ea0ecc302
anaflorescode999 <floresanamariadelosangeles@gmail.com>
2025-05-04 19:06:11 -0300

    cambios u5

---
+++ GrupoD.Prototipo/5. Generar Orden de Entrega/GenerarOrdendeEntregaModelo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrupoD.Prototipo._5._Generar_Orden_de_Entrega
+{
+    public class OrdenEntregaModelo
+    {
+        public int NumeroOrden { get; set; }
+        public string Cliente { get; set; }
+        public DateTime FechaEntrega { get; set; }
+        public long CUIL { get; set; }
+    }
+
+}
+
