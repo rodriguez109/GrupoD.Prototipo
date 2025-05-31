59b7a52a231f3e497ea9225f0ca96171449d7cb8
apanitsch <apanitsch@gmail.com>
2025-05-31 16:04:43 -0300

    Ejemplo modelo en orden de seleccion

--- GrupoD.Prototipo/2. GenerarOrdenSeleccion/GenerarOrdenSeleccionModelo.cs
+++ GrupoD.Prototipo/2. GenerarOrdenSeleccion/GenerarOrdenSeleccionModelo.cs
@@ -1,4 +1,5 @@
-﻿using GrupoD.Prototipo.CDU2._GenerarOrdenSeleccion;
+﻿using GrupoD.Prototipo.Almacenes;
+using GrupoD.Prototipo.CDU2._GenerarOrdenSeleccion;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,8 +10,12 @@ namespace GrupoD.Prototipo._2._GenerarOrdenSeleccion
 {
     class GenerarOrdenSeleccionModelo
     {
-        public List<OrdenesDePreparacion> OrdenesPreparacionDisponibles { get; set; }
+        public List<OrdenesDePreparacion> OrdenesPreparacionDisponibles { get; } = new();
+
+        //Esta hay que borrarla y ver cómo reemplazar su uso en el formulario en cada caso
         public List<OrdenesDeSeleccion> OrdenesDeSeleccion { get; set; }  // Agregar esta propiedad
+
+
         public List<OrdenesDePreparacion> OrdenesAgregadas { get; set; } // Lista de órdenes ya agregadas
 
 
@@ -23,34 +28,35 @@ namespace GrupoD.Prototipo._2._GenerarOrdenSeleccion
         //Guarda los cambios
 
         public GenerarOrdenSeleccionModelo()
-        {
-            OrdenesPreparacionDisponibles = new List<OrdenesDePreparacion>
+        {            
+            foreach (var ordenEntidad in OrdenDePreparacionAlmacen.OrdenesDePreparacion)
             {
-                new OrdenesDePreparacion(1, "DecoHogar S.A.", DateTime.Today.AddDays(0), 20242093, "Alta"),
-                new OrdenesDePreparacion(2, "MaxiLuz Iluminación SRL", DateTime.Today.AddDays(1), 27242093, "Media"),
-                new OrdenesDePreparacion(3, "MundoFOX S.A.", DateTime.Today.AddDays(2), 30242093, "Baja"),
-                new OrdenesDePreparacion(4, "FullColor Pinturerías SRL", DateTime.Today.AddDays(3), 31242093, "Alta"),
-                new OrdenesDePreparacion(5, "Hogar Urbano S.A.", DateTime.Today.AddDays(4), 36242093, "Media"),
-                new OrdenesDePreparacion(6, "Todo Obra Ferretería SRL", DateTime.Today.AddDays(5), 37242093, "Alta"),
-                new OrdenesDePreparacion(7, "Casa Nova Equipamientos S.A.", DateTime.Today.AddDays(6), 38242093, "Baja"),
-                new OrdenesDePreparacion(8, "OfiMarket SRL", DateTime.Today.AddDays(7), 39242093, "Alta"),
-                new OrdenesDePreparacion(9, "Red Tools S.A.", DateTime.Today.AddDays(8), 40242093, "Media"),
-                new OrdenesDePreparacion(10, "MegaMuebles SRL", DateTime.Today.AddDays(9), 41242093, "Baja"),
-                new OrdenesDePreparacion(11, "ElectroCity S.A.", DateTime.Today.AddDays(10), 42242093, "Alta"),
-                new OrdenesDePreparacion(12, "PlastiSur SRL", DateTime.Today.AddDays(11), 43242093, "Media"),
-                new OrdenesDePreparacion(13, "Tecnoshop S.A.", DateTime.Today.AddDays(12), 44242093, "Baja"),
-                new OrdenesDePreparacion(14, "Urban Market SRL", DateTime.Today.AddDays(13), 45242093, "Alta"),
-                new OrdenesDePreparacion(15, "DecoCentro S.A.", DateTime.Today.AddDays(14), 46242093, "Media"),
-                new OrdenesDePreparacion(16, "Centro Herramientas SRL", DateTime.Today.AddDays(15), 47242093, "Baja"),
-                new OrdenesDePreparacion(17, "FullOffice S.A.", DateTime.Today.AddDays(16), 48242093, "Alta"),
-                new OrdenesDePreparacion(18, "DecorArte SRL", DateTime.Today.AddDays(17), 49242093, "Media"),
-                new OrdenesDePreparacion(19, "EasyTech Distribuciones S.A.", DateTime.Today.AddDays(18), 50242093, "Alta"),
-                new OrdenesDePreparacion(20, "FerreMarket SRL", DateTime.Today.AddDays(19), 51242093, "Baja"),
-            };
+                var cliente = ClienteAlmacen.Clientes.FirstOrDefault(c => c.Numero == ordenEntidad.NumeroCliente);
 
+                string prioridad;
+                if (ordenEntidad.Prioridad == PrioridadEnum.Alta)
+                {
+                    prioridad = "Alta";
+                }
+                else if (ordenEntidad.Prioridad == PrioridadEnum.Media)
+                {
+                    prioridad = "Media";
+                }
+                else
+                {
+                    prioridad = "Baja";
+                }
 
+                var ordenPrep = new OrdenesDePreparacion(
+                    ordenEntidad.Numero, cliente.RazonSocial, ordenEntidad.FechaRetirar, ordenEntidad.DNITransportista, prioridad);
+
+                OrdenesPreparacionDisponibles.Add(ordenPrep);
+            }
 
             OrdenesAgregadas = new List<OrdenesDePreparacion>(); // Inicializar la nueva lista
+
+
+            //Esta hay que borrarla.
             OrdenesDeSeleccion = new List<OrdenesDeSeleccion>();  // Lista de órdenes de selección
         }
 
