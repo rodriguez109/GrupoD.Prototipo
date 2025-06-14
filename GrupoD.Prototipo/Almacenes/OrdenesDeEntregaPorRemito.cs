5c4cd95a420c5f12ed76d730c2a2fbe6128f683b
Lilizp <vilis_123@hotmail.com>
2025-06-14 16:37:24 -0300

    dni transportista

---
+++ GrupoD.Prototipo/Almacenes/OrdenesDeEntregaPorRemito.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrupoD.Prototipo.Almacenes
+{
+    internal class OrdenesDeEntregaPorRemito
+    {
+        public int NumeroRemito { get; set; }
+        public int NumeroOrdenDeEntrega { get; set; }
+    }
+}
