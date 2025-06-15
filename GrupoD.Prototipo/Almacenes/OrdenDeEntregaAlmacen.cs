9ce299d87d28dd1946bca2896caccf6e3f691949
anaflorescode999 <floresanamariadelosangeles@gmail.com>
2025-06-15 17:02:52 -0300

    cambios en almacenOE cu5 para generar OE nueva

--- GrupoD.Prototipo/Almacenes/OrdenDeEntregaAlmacen.cs
+++ GrupoD.Prototipo/Almacenes/OrdenDeEntregaAlmacen.cs
@@ -13,8 +13,13 @@ namespace GrupoD.Prototipo.Almacenes
     internal static class OrdenDeEntregaAlmacen
     {
         private static List<OrdenDeEntregaEntidad> ordenesDeEntrega = new List<OrdenDeEntregaEntidad>();
-        
 
+        public static IReadOnlyCollection<OrdenDeEntregaEntidad> OrdenesDeEntrega => ordenesDeEntrega.AsReadOnly();
+        public static void Grabar()
+        {
+            var datos = JsonSerializer.Serialize(ordenesDeEntrega, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(@"Datos\OrdenDeEntrega.json", datos); //Esta parte lo termina de escribir
+        }
         static OrdenDeEntregaAlmacen()
         {
             if (!File.Exists(@"Datos\OrdenDeEntrega.json")) 
@@ -27,23 +32,23 @@ namespace GrupoD.Prototipo.Almacenes
 
         }
         //Muestro una lista publica con todas las OS para que el resto del sistema consulte ReadOnly
-        public static IReadOnlyCollection<OrdenDeEntregaEntidad> OrdenesDeEntrega => ordenesDeEntrega.AsReadOnly();
+        
 
         //Metodo para grabar los datos al archivo:
         //Aca pasamos la lista de OS a formato JSON (la transformamos a un string con los datos de todas las OS)
 
-        public static void Grabar()
-        {
-            var datos = JsonSerializer.Serialize(ordenesDeEntrega, new JsonSerializerOptions { WriteIndented = true });
-            File.WriteAllText(@"Datos\OrdenDeEntrega.json", datos); //Esta parte lo termina de escribir
-        }
+        
 
         public static void Agregar(OrdenDeEntregaEntidad ordenDeEntrega)
         {
             ordenesDeEntrega.Add(ordenDeEntrega);
-
+            Grabar();
         }
 
+        public static OrdenDeEntregaEntidad Buscar(int numero)
+        {
+            return ordenesDeEntrega.FirstOrDefault(o => o.Numero == numero);
+        }
 
 
         public static void cambiarEstadoOE(int IdOP, EstadoOrdenDeEntregaEnum estado)
